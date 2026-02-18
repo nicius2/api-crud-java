5361e459eb7aeab4fbceb8538215088d73ea0d97
nicius2 <marco.marks06@gmail.com>
2026-02-17 21:41:25 -0300

    feat: add car controller

---
+++ src/main/java/com/crud_simple/controller/CarController.java
@@ -0,0 +1,28 @@
+package com.crud_simple.controller;
+
+import com.crud_simple.dto.CarRequestDto;
+import com.crud_simple.dto.CarResponseDto;
+import com.crud_simple.services.CarServices;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/cars")
+public class CarController {
+
+    private final CarServices carServices;
+
+    public CarController(CarServices services) {
+        this.carServices = services;
+    }
+
+    @PostMapping()
+    public ResponseEntity<CarResponseDto> create(@RequestBody CarRequestDto dto) {
+        CarResponseDto saved = carServices.save(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+}
