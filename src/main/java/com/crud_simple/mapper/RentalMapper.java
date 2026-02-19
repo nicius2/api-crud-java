aa4aa5aaf115b6419729530eab719b91798e895d
nicius2 <marco.marks06@gmail.com>
2026-02-19 08:42:23 -0300

    feat: add mapper to organize DTOs

---
+++ src/main/java/com/crud_simple/mapper/RentalMapper.java
@@ -0,0 +1,28 @@
+package com.crud_simple.mapper;
+
+import com.crud_simple.dto.RentalRequestDto;
+import com.crud_simple.dto.RentalResponseDto;
+import com.crud_simple.entities.Car;
+import com.crud_simple.entities.Rental;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RentalMapper {
+
+    public Rental toEntity(RentalRequestDto dto, Car car) {
+        Rental rental = new Rental();
+        rental.setCar(car);
+        rental.setRendedAt(dto.rentedAt());
+        return rental;
+    }
+
+    public RentalResponseDto toDto(Rental rental) {
+        return new RentalResponseDto(
+                rental.getId(),
+                rental.getCar().getNameCar(),
+                rental.getCar().getPlate(),
+                rental.getRendedAt(),
+                rental.getReturnedAt()
+        );
+    }
+}
